714b5f1680af27c90588085a864c756752d8ac72
Mauricio-Maciel-Soares <mauriciomacielsoares@yahoo.com.br>
2022-10-21 15:48:13 -0300

    shore: req 03 e 04

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,9 +1,26 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file_txt = txt_importer(path_file)
+    std_out = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file_txt),
+        "linhas_do_arquivo": file_txt
+    }
+
+    if std_out not in instance.data:
+        instance.enqueue(std_out)
+        print(std_out, file=sys.stdout)
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if len(instance.data) == 0:
+        print('Não há elementos', file=sys.stdout)
+    else:
+        removed = instance.dequeue()["nome_do_arquivo"]
+        print(f'Arquivo {removed} removido com sucesso', file=sys.stdout)
 
 
 def file_metadata(instance, position):
