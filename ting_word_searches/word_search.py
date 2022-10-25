4c4d0895da237d8efc1ccd2f54b3a74d149befe3
Mauricio-Maciel-Soares <mauriciomacielsoares@yahoo.com.br>
2022-10-25 14:14:42 -0300

    shore: req 06

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,24 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    searched_data = list()
+
+    for index in range(len(instance)):
+        line_list = list()
+        file = instance.search(index)
+
+        for i, content in enumerate(file["linhas_do_arquivo"]):
+            if word.lower() in content.lower():
+                line_list.append({"linha": i + 1})
+
+        if (len(line_list) > 0):
+            data = {
+                "palavra": word,
+                "arquivo": file["nome_do_arquivo"],
+                "ocorrencias": line_list
+            }
+            searched_data.append(data)
+
+    return searched_data
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    return []
